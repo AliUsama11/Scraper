311a1509e6c1bba358c72d6341c037fa0eabfb7d
AliUsama11 <126756718+AliUsama11@users.noreply.github.com>
2023-06-01 09:52:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import streamlit as st
 from bs4 import BeautifulSoup
 import requests
+
 def scrape_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.string
-    paragraph = soup.find('p').text
-    return title, paragraph
+    titles = [title.string for title in soup.find_all('title')]
+    paragraphs = [p.text for p in soup.find_all('p')]
+    return titles, paragraphs
 
 st.title("Data Scraping App")
 url = st.text_input("Enter the URL to scrape")
 
 if st.button("Scrape"):
     if url:
-        title, paragraph = scrape_data(url)
-        st.write(f"Title: {title}")
-        st.write(f"Paragraph: {paragraph}")
+        titles, paragraphs = scrape_data(url)
+        st.write("Titles:")
+        for title in titles:
+            st.write(title)
+        
+        st.write("Paragraphs:")
+        for paragraph in paragraphs:
+            st.write(paragraph)
     else:
         st.warning("Please enter a valid URL.")
