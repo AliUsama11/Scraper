a64a623dd7212e22bb91eb72defd1a1154a8455f
AliUsama11 <126756718+AliUsama11@users.noreply.github.com>
2023-06-01 13:00:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,34 @@ def scrape_specific_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-   
+    # Find the specific element(s) you want to scrape
+    # Replace 'element_tag' with the HTML tag of the specific element
     specific_elements = soup.find_all('element_tag')
 
+    # Extract the desired data from the specific elements
     scraped_data = [element.text for element in specific_elements]
 
     return scraped_data
 
+# Streamlit app layout
+st.set_page_config(layout="wide")
 st.title("Specific Data Scraping App")
-url = st.text_input("Enter the URL to scrape")
 
-if st.button("Scrape"):
-    if url:
+# Sidebar
+st.sidebar.title("Settings")
+url = st.sidebar.text_input("Enter the URL to scrape")
+search_query = st.sidebar.text_input("Enter the search query")
+
+# Main content area
+if st.sidebar.button("Scrape"):
+    if url and search_query:
         scraped_data = scrape_specific_data(url)
-        for data in scraped_data:
-            st.write(data)
+        filtered_data = [data for data in scraped_data if search_query.lower() in data.lower()]
+        if filtered_data:
+            st.write("Scraped Data:")
+            for data in filtered_data:
+                st.write(data)
+        else:
+            st.warning("No matching data found.")
     else:
-        st.warning("Please enter a valid URL.")
+        st.warning("Please enter a valid URL and search query.")
