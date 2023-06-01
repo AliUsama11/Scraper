9f14a2da74c6ede543b97a66d3352d2d1546d9cb
AliUsama11 <126756718+AliUsama11@users.noreply.github.com>
2023-06-01 13:10:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,25 @@ import streamlit as st
 from bs4 import BeautifulSoup
 import requests
 
-def scrape_specific_data(url):
+def scrape_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    titles = [title.string for title in soup.find_all('title')]
+    paragraphs = [p.text for p in soup.find_born('p')]
+    return titles, paragraphs
 
-    # Find the specific element(s) you want to scrape
-    # Replace 'element_tag' with the HTML tag of the specific element
-    specific_elements = soup.find_all('element_tag')
+st.title("Data Scraping App")
+url = st.text_input("Enter the URL to scrape")
 
-    # Extract the desired data from the specific elements
-    scraped_data = [element.text for element in specific_elements]
-
-    return scraped_data
-
-# Streamlit app layout
-st.set_page_config(layout="wide")
-st.title("Specific Data Scraping App")
-
-# Sidebar
-st.sidebar.title("Settings")
-url = st.sidebar.text_input("Enter the URL to scrape")
-search_query = st.sidebar.text_input("Enter the search query")
-
-# Main content area
-if st.sidebar.button("Scrape"):
-    if url and search_query:
-        scraped_data = scrape_specific_data(url)
-        filtered_data = [data for data in scraped_data if search_query.lower() in data.lower()]
-        if filtered_data:
-            st.write("Scraped Data:")
-            for data in filtered_data:
-                st.write(data)
-        else:
-            st.warning("No matching data found.")
+if st.button("Scrape"):
+    if url:
+        titles, paragraphs = scrape_data(url)
+        st.write("Titles:")
+        for title in titles:
+            st.write(title)
+       
+        st.write("Paragraphs:")
+        for paragraph in paragraphs:
+            st.write(paragraph)
     else:
-        st.warning("Please enter a valid URL and search query.")
+        st.warning("Please enter a valid URL.")
